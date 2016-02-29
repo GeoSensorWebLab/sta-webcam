6fc33eb28dae06489997230e32af7d4ce279c6fa
James Badger <james@jamesbadger.ca>
2016-02-29 13:03:55 -0700

    Load cached Thing

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
+var thing = {
+  "@iot.selfLink": "http://chashuhotpot.sensorup.com/OGCSensorThings/v1.0/Things(1205774)",
+  "Datastreams@iot.navigationLink": "../Things(1205774)/Datastreams",
+  "@iot.id": 1205774,
+  "description": "GeoSensorWeb Lab",
+  "Locations@iot.navigationLink": "../Things(1205774)/Locations",
+  "properties": {
+    "openstreetmap@link": "http://www.openstreetmap.org/node/3044668378"
+  },
+  "HistoricalLocations@iot.navigationLink": "../Things(1205774)/HistoricalLocations"
+};
+
+
 export default Ember.Route.extend({
+  model() {
+    return thing;
+  }
 });
