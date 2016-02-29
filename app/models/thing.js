6c80fa5a3dd636d4ce139e3651f116e947b407da
James Badger <james@jamesbadger.ca>
2016-02-29 14:59:32 -0700

    Switch to REST Serializer
    Will make it easier to do linking

--- app/models/thing.js
+++ app/models/thing.js
@@ -1,6 +1,13 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   description: DS.attr('string'),
-  properties: DS.attr()
+  properties: DS.attr(),
+
+  locations: DS.hasMany('location', { async: true }),
+
+  firstLocation: Ember.computed('locations', function() {
+    return this.get('locations.firstObject');
+  })
 });
--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,5 +1,12 @@
 import DS from 'ember-data';
 
-export default DS.JSONSerializer.extend({
-  primaryKey: "@iot.id"
+export default DS.RESTSerializer.extend({
+  primaryKey: '@iot.id',
+
+  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+    var newPayload = {};
+    newPayload[primaryModelClass.modelName] = payload;
+
+    return this._super(store, primaryModelClass, newPayload, id, requestType);
+  }
 });
