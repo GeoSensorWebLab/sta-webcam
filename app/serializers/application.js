a5e31053367b03bfa988e80fd8ad8853f1e59e2e
James Badger <james@jamesbadger.ca>
2016-02-29 15:15:12 -0700

    Add link normalization, array normalization

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,10 +1,37 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
+var inflector = new Ember.Inflector(Ember.Inflector.defaultRules);
+
+function isLink(key) {
+  return key.includes("@iot.navigationLink");
+}
+
+function extractLinks(payload) {
+  payload.links = {};
+  Object.keys(payload).filter(isLink).forEach(function(key) {
+    var linkTo = key.split("@")[0].toLowerCase();
+    payload.links[linkTo] = payload[key];
+    delete payload[key];
+  });
+
+  return payload;
+}
 
 export default DS.RESTSerializer.extend({
   primaryKey: '@iot.id',
 
-  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
     var newPayload = {};
+    var collectionName = inflector.pluralize(primaryModelClass.modelName);
+    newPayload[collectionName] = payload.value;
+
+    return this._super(store, primaryModelClass, newPayload, id, requestType);
+  },
+
+  normalizeFindRecordResponse(store, primaryModelClass, payload, id, requestType) {
+    var newPayload = {};
+    payload = extractLinks(payload);
     newPayload[primaryModelClass.modelName] = payload;
 
     return this._super(store, primaryModelClass, newPayload, id, requestType);
