6b6068997febda4008cee400d6a4537055d10e96
James Badger <james@jamesbadger.ca>
2016-02-29 14:59:17 -0700

    Avoid render for empty properties

--- app/components/properties-object.js
+++ app/components/properties-object.js
@@ -3,14 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   init() {
     var props = this.get("properties");
-    var items = Object.keys(props).map(function(key) {
-      return {
-        key: key,
-        value: JSON.stringify(props[key], null, "  ")
-      };
-    });
 
-    this.set("items", items);
+    if (props !== undefined) {
+      var items = Object.keys(props).map(function(key) {
+        return {
+          key: key,
+          value: JSON.stringify(props[key], null, "  ")
+        };
+      });
+
+      this.set("items", items);
+    }
+    
     this._super.apply(this, arguments);
   }
 });
