c71d185a4edd65623b5c10833eb1865546f63dbe
James Badger <james@jamesbadger.ca>
2016-02-29 15:19:08 -0700

    Display props for Location

--- app/models/location.js
+++ app/models/location.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   description: DS.attr('string'),
+  encodingType: DS.attr('string'),
   location: DS.attr(),
 
   thing: DS.belongsTo('thing', { async: true })
