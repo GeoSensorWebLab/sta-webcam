a863616db65f443daaba56767d90861007da1984
James Badger <james@jamesbadger.ca>
2016-03-01 13:35:23 -0700

    Fix tab active highlight toggle

--- app/components/observation-preview.js
+++ app/components/observation-preview.js
@@ -3,15 +3,23 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement() {
     this.$("button[data-target=feature]").on("click", () => {
-      this.$(".result-preview").hide();
-      this.$(".feature-preview").show();
-      // Toggle visibility of special elements in feature preview
-      this.$(".feature-preview *").trigger("isVisible");
+      if (this.$(".feature-preview").filter(":visible").size() === 0) {
+        this.$(".result-preview").hide();
+        this.$(".feature-preview").show();
+        // Toggle visibility of special elements in feature preview
+        this.$(".feature-preview *").trigger("isVisible");
+
+        this.$("li").toggleClass("active");
+      }
     });
 
     this.$("button[data-target=result]").on("click", () => {
-      this.$(".feature-preview").hide();
-      this.$(".result-preview").show();
+      if (this.$(".result-preview").filter(":visible").size() === 0) {
+        this.$(".feature-preview").hide();
+        this.$(".result-preview").show();
+
+        this.$("li").toggleClass("active");
+      }
     });
   }
 });
