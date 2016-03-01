4e34afae839817b5cbf8d3fc5771a393395e1dcd
James Badger <james@jamesbadger.ca>
2016-03-01 14:23:04 -0700

    Add pub/sub event bus global
    Will be used for managing MQTT subscriptions internally.

--- app/components/datastream-preview.js
+++ app/components/datastream-preview.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  init() {
+    this.addObserver("datastream", this, "modelLoaded");
+    this.modelLoaded();
+
+    this._super.apply(this, arguments);
+  },
+
+  // Send an event to the global bus to subscribe to an MQTT stream
+  modelLoaded() {
+    var id = this.get("datastream.id");
+    this.globalEvents.trigger("mqtt:subscribe", `Datastreams(${id})/Observations`);
+  },
+
+  willDestroy() {
+    var id = this.get("datastream.id");
+    this.globalEvents.trigger("mqtt:unsubscribe", `Datastreams(${id})/Observations`);
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  init() {
+    this.globalEvents.on('mqtt:subscribe', this, 'subscribe');
+    this._super.apply(this, arguments);
+  },
+
   model() {
     return this.store.findRecord("thing", 1205774);
+  },
+
+  subscribe(subPath) {
+    console.log("Subscribe", subPath);
+
+    
   }
 });
