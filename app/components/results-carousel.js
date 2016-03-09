b87a3f92a73b6281257b31a5164b0344ef717bac
James Badger <james@jamesbadger.ca>
2016-03-08 19:00:01 -0700

    Add prev/next function to results-carousel

--- app/components/results-carousel.js
+++ app/components/results-carousel.js
@@ -2,24 +2,37 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['results-carousel'],
+  isLoading: true,
 
   observations: Ember.computed('datastream.observations', function () {
     return this.get('datastream.observations').sortBy('phenomenonTime');
   }),
 
-  hasNextObservation: Ember.computed('observations', 'activeObservation', function() {
-    var observations = this.get('observations');
-    var active = this.get('activeObservation');
-    var index = observations.indexOf(active);
-    return observations.objectAt(index + 1) !== undefined;
+  hasNextObservation: Ember.computed('observations', 'active-observation', function() {
+    return this.getActiveOffset(1) !== undefined;
   }),
 
-  hasPreviousObservation: Ember.computed('observations', 'activeObservation', function() {
-    var observations = this.get('observations');
-    var active = this.get('activeObservation');
-    var index = observations.indexOf(active);
-    return observations.objectAt(index - 1) !== undefined;
+  hasPreviousObservation: Ember.computed('observations', 'active-observation', function() {
+    return this.getActiveOffset(-1) !== undefined;
   }),
+
+  actions: {
+    loadNext() {
+      var next = this.getActiveOffset(1);
+      if (next) {
+        this.set('active-observation', next);
+        this.set('isLoading', true);
+      }
+    },
+
+    loadPrevious() {
+      var previous = this.getActiveOffset(-1);
+      if (previous) {
+        this.set('active-observation', previous);
+        this.set('isLoading', true);
+      }
+    }
+  },
 
   init() {
     this.get('datastream.observations');
@@ -27,10 +40,25 @@ export default Ember.Component.extend({
     this._super.apply(this, arguments);
   },
 
+  didInsertElement() {
+    this.$('img').on('load', () => {
+      this.set('isLoading', false);
+    });
+  },
+
   // set the initially active observation after the observation have been
   // fetched. This only happens once.
   initActiveObservation() {
-    this.set('activeObservation', this.get('observations.lastObject'));
+    this.set('active-observation', this.get('observations.lastObject'));
     this.removeObserver('datastream.observations', this, 'initActiveObservation');
+  },
+
+  // Return the observation in the observations array that is offset by index
+  // "offset"
+  getActiveOffset(offset) {
+    var observations = this.get('observations');
+    var active = this.get('active-observation');
+    var index = observations.indexOf(active);
+    return observations.objectAt(index + offset);
   }
 });
